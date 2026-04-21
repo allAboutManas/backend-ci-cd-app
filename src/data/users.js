9b5b690f9ebd2ef5333251694b9ec69465de12ff
Manas Raj <rm.manasraj@gmail.com>
2026-04-21 17:53:21 +0530

    User API + tests passing locally

--- src/data/users.js
+++ src/data/users.js
@@ -1,14 +1,12 @@
-import express from 'express';
-import {
-  getAllUsers,
-  getUserById,
-  createUser
-} from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/', getAllUsers);
-router.get('/:id', getUserById);
-router.post('/', createUser);
-
-export default router;
+export const users = [
+  {
+    id: 1,
+    name: 'Manas',
+    email: 'manas@example.com'
+  },
+  {
+    id: 2,
+    name: 'Tony',
+    email: 'tony@example.com'
+  }
+];
